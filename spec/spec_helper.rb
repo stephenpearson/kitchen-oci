e405882128d49ec4cbb149a503006c0f7c7cf5a1
justintsteele <steeele.justin@gmail.com>
2024-03-26 09:54:31 -0400

    test: add no_proxy to stubbed environment for api with proxy spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -465,7 +465,7 @@ end
 
 RSpec.shared_context "proxy", :proxy do |rspec|
   before do
-    stub_const("ENV", ENV.to_hash.merge("http_proxy" => "http://myfakeproxy.com"))
+    stub_const("ENV", ENV.to_hash.merge({ "http_proxy" => "http://myfakeproxy.com", "no_proxy" => ".myfakedomain.com" }))
     allow(OCI::ApiClientProxySettings).to receive(:new).with("myfakeproxy.com", 80).and_return(proxy_settings)
   end
   let(:proxy_settings) { OCI::ApiClientProxySettings.new("myfakeproxy.com", 80) }
