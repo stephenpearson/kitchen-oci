51cea193621f8d690a79054878b1f75ca9c7d4a7
Justin Steele <justin.steele@oracle.com>
2025-01-15 23:15:18 -0500

    Revert "chore: add license to new spec helper files"
    This reverts commit 53da1516513d3d5c8ae48c71bb7784b1130a0f79.

--- spec/spec_helper/paravirtual_helper.rb
+++ spec/spec_helper/paravirtual_helper.rb
@@ -1,22 +1,5 @@
 # frozen_string_literal: true
 
-#
-# Author:: Justin Steele (<justin.steele@oracle.com>)
-#
-# Copyright (C) 2024, Stephen Pearson
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 RSpec.shared_context "paravirtual", :paravirtual do
   let(:pv_volume_ocid) { "ocid1.volume.oc1.fake.aaaaaaaaaabcdefghijklmnopqrstuvwxyz67890" }
   let(:pv_attachment_ocid) { "ocid1.volumeattachment.oc1.fake.aaaaaaaaaabcdefghijklmnopqrstuvwxyz67890" }
