e63b7f70aa2d502290fce7e9045724d2a90d3ee2
Justin Steele <justin.steele@oracle.com>
2025-01-15 23:15:20 -0500

    Revert "refactor: break up spec_helper"
    This reverts commit 9c27ec8867539107234b6c8e87c5f9241d54a01c.

--- spec/spec_helper/api_helper.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.shared_context "api", :common do |rspec|
-  let(:oci_config) { class_double(OCI::Config) }
-  let(:driver_config) { {} }
-end
--- spec/spec_helper/proxy_helper.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.shared_context "proxy", :proxy do |rspec|
-  before do
-    stub_const("ENV", ENV.to_hash.merge({ "http_proxy" => "http://myfakeproxy.com", "no_proxy" => ".myfakedomain.com" }))
-    allow(OCI::ApiClientProxySettings).to receive(:new).with("myfakeproxy.com", 80).and_return(proxy_settings)
-  end
-  let(:proxy_settings) { OCI::ApiClientProxySettings.new("myfakeproxy.com", 80) }
-end
