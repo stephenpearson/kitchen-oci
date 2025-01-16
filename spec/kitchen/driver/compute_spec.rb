cff13c7024d27266a75b6d70d4bb9f44c17a6787
Justin Steele <justin.steele@oracle.com>
2025-01-15 23:15:11 -0500

    Revert "fix: windows instance spec should not expect ssh_authorized_keys"
    This reverts commit 2d3f4a70dcf213bf1d3f0307df23687c1a90b730.

--- spec/kitchen/driver/compute_spec.rb
+++ spec/kitchen/driver/compute_spec.rb
@@ -75,6 +75,7 @@ describe Kitchen::Driver::Oci::Models::Compute do
 
       let(:instance_metadata) do
         {
+          "ssh_authorized_keys" => ssh_pub_key,
           "user_data" => "FaKeUsErDaTa",
           "hostclass" => "foo",
         }
