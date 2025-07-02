7acb9476da3610dd7481e429674b7821866071a3
Justin Steele <justin.steele@oracle.com>
2025-07-02 09:17:03 -0400

    feat: add post launch check for legacy metadata endpoints being enabled

--- lib/kitchen/driver/oci/mixin/actions.rb
+++ lib/kitchen/driver/oci/mixin/actions.rb
@@ -33,7 +33,8 @@ module Kitchen
             state_details = inst.launch
             state.merge!(state_details)
             instance.transport.connection(state).wait_until_ready
-            instance_options(state, inst) if config[:instance_type] == "compute"
+            instance_options(state, inst)
+            are_legacy_imds_endpoints_disbled?(state, inst)
           end
 
           # Executes the post script on the instance.
@@ -84,9 +85,19 @@ module Kitchen
           # Attempts to disable IMDSv1 even if not explicitly specified in the config. This is in line with current security guidance from OCI.
           # Acts as a guard for setting instance options.
           def instance_options?
+            return false unless config[:instance_type] == "compute"
+
             config[:instance_options].merge!(are_legacy_imds_endpoints_disabled: true) unless config[:instance_options].key?(:are_legacy_imds_endpoints_disabled)
             # Basically tell me if there's more stuff in there than `are_legacy_imds_endpoints_disabled: false`. If so, then proceed to setting it.
             config[:instance_options].reject { |o, v| o == :are_legacy_imds_endpoints_disabled && !v }.any?
+          end
+
+          # Checks if legacy metadata is disabled.
+          def are_legacy_imds_endpoints_disbled?(state, inst)
+            return unless config[:instance_type] == "compute"
+
+            imds = inst.api.compute.get_instance(state[:server_id]).data.instance_options.are_legacy_imds_endpoints_disabled
+            inst.logger.warn("Legacy IMDSv1 endpoint is enabled.") unless imds
           end
 
           # Reboots an instance.
